675c4aa9dca65d9295219f7632bf83c632b737e7
Emm-Hz <emmnueljose59@gmail.com>
2022-03-04 13:24:03 -0400

    Exersises day 1

---
+++ day-1/datatypes.js
@@ -0,0 +1,7 @@
+console.log(typeof text);
+console.log(typeof bool);
+console.log(typeof unknow);
+console.log(typeof blank);
+
+console.log(`I am ${myAge} years old.`);
+console.log(`You are ${yourAge} years old.`);
---
+++ day-1/variable.js
@@ -0,0 +1,24 @@
+var text = '';
+var bool = false;
+var unknow;
+var blank = null;
+
+var a;
+var b;
+var c;
+var d;
+
+var e = 1;
+var f = 2;
+var g = 3;
+var g = 4;
+
+var name = 'Emmanuel';
+var lastName = 'Higuera';
+var maritalStatus = 'Single';
+var country = 'Venezuela';
+var age = 26;
+
+var [fName,lName,mStatus,coun,ag] = ['Emmanuel','Higuera','Single','Venezuela',26]
+
+var [myAge,yourAge] = [25,30]
